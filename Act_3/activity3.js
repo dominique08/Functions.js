16684816e275e4deec72b11bc44561c7068cf549
Dominique Galigao <112061913+dominique08@users.noreply.github.com>
2022-10-21 17:19:00 +0800

    Add files via upload
    Third commit

---
+++ Act_3/activity3.js
@@ -0,0 +1,7 @@
+
+function sot(str){
+    //put string in lowercase and remove spaces from it.
+        str=str.toLowerCase(). replace(/[^a-zA-Z0-9]+/g,'');
+    //sort the string.
+        return str.split("").sort().join("");
+    }console.log(sot("university"));
